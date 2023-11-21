e892ed65cb62dbf3b1daa73694d46beab9343e29
Jeeva <jpranesh14@gmail.com>
2023-11-21 12:13:34 +0530

    get_emp update_emp fix

--- app/schemas/request.py
+++ app/schemas/request.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
-
-from app.schemas.employees import EmployeeBase
+from pydantic import BaseModel, validator
+from fastapi import HTTPException
+from app.schemas.employees import EmployeeBase, BankDetails, Address, GovtIDProofs
 from app.schemas.salary import SalaryBase, SalaryAdvanceBase
 from app.schemas.leave import LeaveBase, PermissionBase
 from app.schemas.loan import LoanBase
@@ -15,19 +15,46 @@ from typing import Optional
 exclude_fields_for_employee_update = ["employee_id", "email"]
 
 
+# Define a phone number validator function
+def validate_phone_number(cls, value, field):
+    if field.name == "phone_number" and value is not None:
+        if not (value.isdigit() and len(value) == 10):
+            raise ValueError("Phone number must be a 10-digit number")
+    return value
+
+
 class EmployeeCreateRequest(EmployeeBase):
     # employees: Employee
     pass
 
 
-EmployeeUpdateRequest = create_model(
-    "EmployeeUpdateRequest",
-    **{
-        name: (field, ...)
-        for name, field in EmployeeBase.__annotations__.items()
-        if name not in exclude_fields_for_employee_update
-    }
-)
+# EmployeeUpdateRequest = create_model(
+#     "EmployeeUpdateRequest",
+#     **{
+#         name: (Optional[EmployeeBase.__annotations__[name]], None)
+#         for name, field in EmployeeBase.__annotations__.items()
+#         if name not in exclude_fields_for_employee_update
+#     }
+# )
+
+
+class EmployeeUpdateRequest(BaseModel):
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    department: Optional[str] = None
+    designation: Optional[str] = None
+    bank_details: Optional[BankDetails] = None
+    address: Optional[Address] = None
+    govt_id_proofs: Optional[GovtIDProofs] = None
+    # TODO: Father/Husband Phone Number (Optional) - decide on how to store this
+
+    @validator("phone")
+    def phone_validator(cls, v):
+        if len(v) != 10:
+            raise HTTPException(
+                status_code=400, detail="Phone number must be 10 digits"
+            )
+        return v
 
 
 class SalaryCreateRequest(SalaryBase):
